57d3c5954067e276192cba377949fb6025497c73
tatendabakozw <tatendabakozw@gmail.com>
2021-09-20 19:36:11 +0200

    contact page dark mode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import useDarkMode from './useDarkMode';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './pages/Home';
 import Contact from './pages/contact/Contact';
-// import NotFound from './pages/notfound/NotFound';
+import NotFound from './pages/notfound/NotFound';
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/" component={Home} />
-        {/* <Route component={NotFound} /> */}
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
